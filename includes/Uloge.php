00baf29ca3ddb73398d52c56308407b87e6f08e7
al3xx9 <djoricaleksandar9@gmail.com>
2023-01-19 22:16:41 +0100

    added uloge

---
+++ includes/Uloge.php
@@ -0,0 +1,14 @@
+<?php
+class Uloge
+{
+    public static function get(): array
+    {
+        return [
+            'Back-end developer',
+            'Front-end developer',
+            'Project manager',
+            'Designer',
+            'QA'
+        ];
+    }
+}
