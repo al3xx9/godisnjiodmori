14e796bd320814805f7e0eae0883bcbaaaf82a74
al3xx9 <djoricaleksandar9@gmail.com>
2023-01-19 23:39:39 +0100

    listanje zahteva zaposlenog

--- index.php
+++ index.php
@@ -5,9 +5,31 @@ check_is_logged_in();
 
 $database = Database::getInstance();
 
+$iskorisceni_odmori = (array) $database->get()['iskorisceni_odmori'];
+$zahtevi_za_odmor = (array) $database->get()['zahtevi_za_odmor'];
+$odbijeni_zahtevi = (array) $database->get()['odbijeni_zahtevi'];
+
 if ($_SESSION['username'] != 'admin') {
     $radnik = get_radnik_by_username($_SESSION['username']);
     $rasplozivo_dana_odmora = BrojDanaOdmora::izracunaj($radnik);
+    $odobreni_zahtevi = [];
+    foreach ($iskorisceni_odmori as $godina => $iskorisceni_odmori_po_godini) {
+        if (isset($iskorisceni_odmori_po_godini[$radnik->id])) {
+            foreach ((array) $iskorisceni_odmori_po_godini[$radnik->id] as $odmor_item) {
+                $odobreni_zahtevi[] = $odmor_item;
+            }
+        }
+    }
+
+    $zahtevi_na_cekanju = [];
+    if (isset($zahtevi_za_odmor[$radnik->id])) {
+        $zahtevi_na_cekanju = $zahtevi_za_odmor[$radnik->id];
+    }
+
+    $moji_odbijeni_zahtevi = [];
+    if (isset($odbijeni_zahtevi[$radnik->id])) {
+        $moji_odbijeni_zahtevi = $odbijeni_zahtevi[$radnik->id];
+    }
 }
 ?>
 <h1>Sistem za elektronsku evidenciju godisnjih odmora</h1>
@@ -30,8 +52,44 @@ if ($_SESSION['username'] == 'admin') {
         } ?></p>
     <p><a href="unesi-zahtev.php">Unesite novi zahtev</a></p>
     <h3>Vasi odobreni zahtevi</h3>
+    <table style="width:100%" cellpadding="0" cellspacing="0" border="1">
+        <tr>
+            <th>Datum pocetka</th>
+            <th>Broj dana</th>
+        </tr>
+        <?php foreach ($odobreni_zahtevi as $odobren_zahtev) { ?>
+            <tr>
+                <td><?php echo $odobren_zahtev['datum_pocetka']; ?></td>
+                <td><?php echo $odobren_zahtev['broj_dana']; ?></td>
+            </tr>
+        <?php } ?>
+    </table>
     <h3>Vasi odbijeni zahtevi</h3>
+    <table style="width:100%" cellpadding="0" cellspacing="0" border="1">
+        <tr>
+            <th>Datum pocetka</th>
+            <th>Broj dana</th>
+        </tr>
+        <?php foreach ($moji_odbijeni_zahtevi as $moj_odbijen_zahtev) { ?>
+            <tr>
+                <td><?php echo $moj_odbijen_zahtev['datum_pocetka']; ?></td>
+                <td><?php echo $moj_odbijen_zahtev['broj_dana']; ?></td>
+            </tr>
+        <?php } ?>
+    </table>
     <h3>Vasi zahtevi na cekanju</h3>
+    <table style="width:100%" cellpadding="0" cellspacing="0" border="1">
+        <tr>
+            <th>Datum pocetka</th>
+            <th>Broj dana</th>
+        </tr>
+        <?php foreach ($zahtevi_na_cekanju as $zahtev_na_cekanju) { ?>
+            <tr>
+                <td><?php echo $zahtev_na_cekanju['datum_pocetka']; ?></td>
+                <td><?php echo $zahtev_na_cekanju['broj_dana']; ?></td>
+            </tr>
+        <?php } ?>
+    </table>
 <?php
 }
 ?>
--- unesi-zahtev.php
+++ unesi-zahtev.php
@@ -3,11 +3,32 @@ require 'config.php';
 
 check_is_logged_in();
 
+$error = false;
+
 $database = Database::getInstance();
 
 if ($_SESSION['username'] == 'admin') {
     redirect('index.php');
 }
+
+$radnik = get_radnik_by_username($_SESSION['username']);
+$rasplozivo_dana_odmora = BrojDanaOdmora::izracunaj($radnik);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+}
 ?>
 <h1>Sistem za elektronsku evidenciju godisnjih odmora</h1>
+<?php if ($error) { ?>
+    <p style="color:red"><?php echo $error; ?></p>
+<?php } ?>
 <h3>Unosenje novog zahteva za godisnji odmor</h3>
+<p><b>Raspoloziv broj dana odmora: <?php echo htmlspecialchars($rasplozivo_dana_odmora); ?></p>
+<form method="post" action="<?php echo htmlspecialchars($www); ?>unesi-zahtev.php">
+    <p><b>Datum pocetka</b></p>
+    <input type="text" name="datum_pocetka" value="" placeholder="YYYY-MM-DD" />
+    <br />
+    <p><b>Broj dana</b></p>
+    <input type="number" name="broj_dana" value="" placeholder="" />
+    <br />
+    <button type="submit">Save</button>
+</form>
