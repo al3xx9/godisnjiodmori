2fc8fa28ebee0d81543ed0353a89fea09afa5bed
al3xx9 <djoricaleksandar9@gmail.com>
2023-01-19 21:37:12 +0100

    skeleton

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+ini_set('log_errors', true);
+ini_set('error_log', 'php_errors.log');
+
+require 'includes/functions.php';
+require 'includes/Database.php';
